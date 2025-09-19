a866f339f4af4be8264db511f6e2900b7b9ae4a3
aksharagyana <93100395+aksharagyana@users.noreply.github.com>
2025-09-19 07:29:52 +0100

    Update aks.sh

--- aks.sh
+++ aks.sh
@@ -1,2 +1,7 @@
 az aks show -g <rg> -n <aks-cluster> \
   --query "identity" -o json
+
+
+# (used for VM (node) to talk to Azure resources)
+az aks show -g <rg> -n <aks-cluster> \
+  --query "identityProfile.kubeletidentity" -o json
