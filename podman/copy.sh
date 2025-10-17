83e21c5daa9317249a3b0942561554936db2a4b5
aksharagyana <93100395+aksharagyana@users.noreply.github.com>
2025-10-17 11:57:27 +0100

    Create copy.sh

---
+++ podman/copy.sh
@@ -0,0 +1,28 @@
+. Basic Plan
+
+Use the quay.io/skopeo/stable container image to run skopeo like this:
+
+docker run --rm \
+  -v /your/local/dir:/output \
+  quay.io/skopeo/stable \
+  copy docker://nginx:latest docker-archive:/output/nginx.tar:nginx:latest
+  
+Let’s say you want to save the nginx image tarball to:
+
+mkdir -p /opt/images
+docker run --rm \
+  -v /opt/images:/output \
+  quay.io/skopeo/stable \
+  copy docker://nginx:latest docker-archive:/output/nginx.tar:nginx:latest
+
+Load the Image into Docker
+
+docker load < /opt/images/nginx.tar
+docker run -it nginx:latest
+
+Want to Ignore TLS Certs?
+
+docker run --rm \
+  -v /opt/images:/output \
+  quay.io/skopeo/stable \
+  copy --tls-verify=false docker://<your-registry>/nginx:latest docker-archive:/output/nginx.tar:nginx:latest
